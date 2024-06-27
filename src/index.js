94621786eea3c9cee0062711b7775786a84398a0
Fernando Minardi <fminardi@sodep.com.py>
2024-06-27 13:37:18 -0400

    Nuevo método de importación en el index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,4 @@
-import {
-  WEEKDAYS,
-  MONTHS,
-  getToday,
-  getNow,
-  getDayOfWeekMonthYear,
-} from './date';
-
-import { getAtlasToken, getData } from './auth';
-
-import {
-  getPropsInfo,
-  getCurrentStep,
-  getCorrelationId,
-  buildUrls,
-  getInitialValues,
-  getGlobalVariables,
-} from './misc';
-
-import { convertToBase64, downloadPdf } from './file';
-
-export {
-  WEEKDAYS,
-  MONTHS,
-  getToday,
-  getNow,
-  getDayOfWeekMonthYear,
-  getAtlasToken,
-  getData,
-  getPropsInfo,
-  getCurrentStep,
-  getCorrelationId,
-  buildUrls,
-  getInitialValues,
-  getGlobalVariables,
-  convertToBase64,
-  downloadPdf,
-};
+export * from './date';
+export * from './auth';
+export * from './misc';
+export * from './file';
