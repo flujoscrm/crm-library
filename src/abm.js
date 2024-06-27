33a6dd9ea69a1ef72c44a36f56af383de9f52823
xHenrySx <enrisal2000@gmail.com>
2024-06-27 15:50:58 -0400

    Dispone el nuevo dist

--- src/abm.js
+++ src/abm.js
@@ -2,7 +2,7 @@ import { getInitialValues } from './misc';
 import { getData } from './auth';
 import { getToday } from './date';
 
-const getFormFields = async (props, form) => {
+export const getFormFields = async (props, form) => {
   const { registerUrl, token, tokenUrl } = getInitialValues(props);
   const payload = {
     nombreObjeto: 'CONF_CAMPOS',
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export * from './date';
 export * from './auth';
 export * from './misc';
 export * from './file';
+export * from './abm';
